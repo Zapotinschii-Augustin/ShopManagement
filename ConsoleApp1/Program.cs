98a9f3a1c851202a2fdb753e8b352a18699ce8de
Zapotinshii Augustin <Zapotinshii Augustin@DESKTOP-48VJOAK>
2024-03-09 16:28:15 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shopName = ConfigurationManager.AppSettings.Get("shop_name");
+            Console.WriteLine(shopName);
+            Console.ReadKey();
+        }
+    }
+}
